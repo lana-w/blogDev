2c0957689db11d1becb9a0a17d6acadc0e40df82
Lana W <lanawehbeh12@gmail.com>
2023-12-30 11:04:58 -0500

    Addition of Blog Feed Page

---
+++ src/app/blogFeed.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function blogFeed (){
+
+    return (
+        <main>
+            <Link href="/page">
+                <a>Back to Post</a>
+            </Link>
+        
+        </main>
+    );
+}
