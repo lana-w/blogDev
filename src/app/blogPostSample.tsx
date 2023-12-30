c81aab66e0f5728368733cbe07985566501ea0ea
Lana W <lanawehbeh12@gmail.com>
2023-12-30 11:05:15 -0500

    Addition of blogPostSample page

---
+++ src/app/blogPostSample.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function blogPostSample (){
+
+    return (
+        <main>
+
+
+
+            <Link href="/blog_feed">
+                <a>Back to Feed</a>
+            </Link>
+        
+        </main>
+    );
+}
